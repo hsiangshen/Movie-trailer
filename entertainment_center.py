8aa37e05e1d098304b2cc77a51f176f9f4c120fe
Hsiang Shen <hsiangshen@yahoo.com>
2016-07-16 14:05:04 -0700

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,42 +1,57 @@
-
-#call phyton code fresh_tomatoes for browser css format and opening movie trailer, media for class Movie
+# call phyton code fresh_tomatoes for browser css format
+# and opening movie trailer
+# call media for class Movie
 import fresh_tomatoes
 import media
 
-sword_rain = media.Movie("Reign of Assassins",
-    "A narration tells of a legendary Indian Buddhist monk, Bodhi and the belief that his mummified remains has mystical powers.",
+# creat Movie object
+sword_rain = media.Movie(
+    "Reign of Assassins",
+    "A tells of a legendary monk that his mummified remains has mystical powers.",
     "https://upload.wikimedia.org/wikipedia/en/6/64/Reign-of-Assassins.jpg",
-    "https://www.youtube.com/watch?v=IczLF2rcs10")
+    "https://www.youtube.com/watch?v=IczLF2rcs10"
+    )
 
-tais_toi = media.Movie("Ruby and Quentin",
+tais_toi = media.Movie(
+    "Ruby and Quentin",
     "Two guys escaped from a jail",
     "https://upload.wikimedia.org/wikipedia/en/1/1f/Tais-toi_poster.jpg",
-    "https://www.youtube.com/watch?v=BSFfMs9teMY")
+    "https://www.youtube.com/watch?v=BSFfMs9teMY"
+    )
 
-red_turle = media.Movie("La Tortue rouge",
+red_turle = media.Movie(
+    "La Tortue rouge",
     "a red tutler journey",
     "https://upload.wikimedia.org/wikipedia/en/f/fe/The_Red_Turtle.png",
-    "https://www.youtube.com/watch?v=y3uYequDQqc")
+    "https://www.youtube.com/watch?v=y3uYequDQqc"
+    )
 
-hotarubi_no_morie = media.Movie("Into the forest of firefly light",
+hotarubi_no_morie = media.Movie(
+    "Into the forest of firefly light",
     "A young girl had a strange relationship with a spirit",
     "https://upload.wikimedia.org/wikipedia/en/f/fd/Hotarubi_no_Mori_e_%28manga_cover%29.jpg",
-    "https://www.youtube.com/watch?v=TFvezWFnaHw")
+    "https://www.youtube.com/watch?v=TFvezWFnaHw"
+    )
 
-time_of_eve = media.Movie("The time of Eve",
+time_of_eve = media.Movie(
+    "The time of Eve",
     "In the near furture, the relationship between people and robort become completcate.",
     "https://upload.wikimedia.org/wikipedia/en/f/fa/Ivu_no_Jikan_poster.jpeg",
-    "https://www.youtube.com/watch?v=5CrNNs-37As")
+    "https://www.youtube.com/watch?v=5CrNNs-37As"
+    )
 
-my_neighbor_totoro = media.Movie("My neighbor Totoro",
-    "The adventure for 2 little girls after their family moved into an old country house.",
+my_neighbor_totoro = media.Movie(
+    "My neighbor Totoro",
+    "The adventure of 2 little girls after their family moved into an old country house.",
     "https://upload.wikimedia.org/wikipedia/en/0/02/My_Neighbor_Totoro_-_Tonari_no_Totoro_%28Movie_Poster%29.jpg",
-    "https://www.youtube.com/watch?v=92a7Hj0ijLs")
+    "https://www.youtube.com/watch?v=92a7Hj0ijLs"
+    )
 
-
+# movie lists for fresh_tomatoes to read
 movies = [
             sword_rain, tais_toi, red_turle, 
             hotarubi_no_morie, time_of_eve, my_neighbor_totoro
         ]
 fresh_tomatoes.open_movies_page(movies)
+
 
